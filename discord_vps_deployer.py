783e8df6a6906b6baa7b6ea72c32ad331eaf04b5
lonelyexams <furvisiuu6@gmail.com>
2025-03-03 18:37:17 +0800

    Update discord_vps_deployer.py

--- discord_vps_deployer.py
+++ discord_vps_deployer.py
@@ -9,8 +9,9 @@ ALLOWED_CHANNEL_IDS = [123456789012345678]  # Replace with your allowed channel 
 ALLOWED_ROLE_IDS = [987654321098765432]     # Replace with your allowed role IDs
 TOKEN = "YOUR_DISCORD_BOT_TOKEN"            # Replace with your Discord bot token
 
-# Supported OS options
+# Supported OS options (40 operating systems)
 SUPPORTED_OS = {
+    # Original 10 OS
     "ubuntu": "https://releases.ubuntu.com/22.04/ubuntu-22.04.3-live-server-amd64.iso",
     "debian": "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.1.0-amd64-netinst.iso",
     "centos": "http://isoredirect.centos.org/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso",
@@ -20,7 +21,43 @@ SUPPORTED_OS = {
     "kali": "https://cdimage.kali.org/kali-2023.3/kali-linux-2023.3-installer-amd64.iso",
     "opensuse": "https://download.opensuse.org/distribution/leap/15.5/iso/openSUSE-Leap-15.5-DVD-x86_64.iso",
     "rocky": "https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.2-x86_64-dvd.iso",
-    "freebsd": "https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/13.2/FreeBSD-13.2-RELEASE-amd64-dvd1.iso"
+    "freebsd": "https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/13.2/FreeBSD-13.2-RELEASE-amd64-dvd1.iso",
+
+    # Additional 30 OS
+    "gentoo": "https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20231008T170153Z/livegui-amd64-20231008T170153Z.iso",
+    "manjaro": "https://download.manjaro.org/kde/22.1.0/manjaro-kde-22.1.0-230529-linux61.iso",
+    "mint": "https://mirrors.edge.kernel.org/linuxmint/iso/stable/21.2/linuxmint-21.2-cinnamon-64bit.iso",
+    "popos": "https://pop-iso.sfo2.cdn.digitaloceanspaces.com/22.04/amd64/intel/7/pop-os_22.04_amd64_intel_7.iso",
+    "zorin": "https://mirrors.edge.kernel.org/zorinos-isos/16/Zorin-OS-16.2-Core-64-bit.iso",
+    "elementary": "https://ams3.dl.elementary.io/download/MTY2NzI2NzI2OA==/elementaryos-7.0-stable.20230308.iso",
+    "deepin": "https://cdimage.deepin.com/releases/23/Deepin-23-Beta2-amd64.iso",
+    "mxlinux": "https://sourceforge.net/projects/mx-linux/files/Final/Xfce/MX-23_x64.iso/download",
+    "slackware": "https://mirrors.slackware.com/slackware/slackware64-15.0/iso/slackware64-15.0-install-dvd.iso",
+    "void": "https://alpha.de.repo.voidlinux.org/live/current/void-live-x86_64-20230628.iso",
+    "nixos": "https://channels.nixos.org/nixos-23.05/latest-nixos-plasma5-x86_64-linux.iso",
+    "clear": "https://cdn.download.clearlinux.org/releases/38510/clear/clear-38510-live.iso",
+    "tails": "https://tails.boum.org/install/vm/Tails.amd64.5.18.1-0.img",
+    "parrot": "https://deb.parrot.sh/parrot/iso/5.3/Parrot-security-5.3_amd64.iso",
+    "blackarch": "https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-full-2023.09.01-x86_64.iso",
+    "qubes": "https://mirrors.edge.kernel.org/qubes/iso/Qubes-R4.1.2-x86_64.iso",
+    "reactos": "https://sourceforge.net/projects/reactos/files/ReactOS/0.4.15/ReactOS-0.4.15-iso.zip/download",
+    "haiku": "https://download.haiku-os.org/nightly-images/x86_64/haiku-nightly-hrev57681-x86_64-anyboot.zip",
+    "solus": "https://downloads.getsol.us/4.4/Solus-4.4-Budgie.iso",
+    "puppy": "https://distro.ibiblio.org/puppylinux/puppy-fossa/fossapup64-9.5.iso",
+    "tinycore": "http://tinycorelinux.net/13.x/x86_64/release/TinyCorePure64-13.0.iso",
+    "antix": "https://sourceforge.net/projects/antix-linux/files/Final/antiX-23/antiX-23_x64-full.iso/download",
+    "bodhi": "https://sourceforge.net/projects/bodhilinux/files/7.0.0/bodhi-7.0.0-64.iso/download",
+    "peppermint": "https://peppermintos.com/iso/Peppermint-11-20230703-amd64.iso",
+    "lubuntu": "https://cdimage.ubuntu.com/lubuntu/releases/22.04/release/lubuntu-22.04.3-desktop-amd64.iso",
+    "kubuntu": "https://cdimage.ubuntu.com/kubuntu/releases/22.04/release/kubuntu-22.04.3-desktop-amd64.iso",
+    "xubuntu": "https://cdimage.ubuntu.com/xubuntu/releases/22.04/release/xubuntu-22.04.3-desktop-amd64.iso",
+    "ubuntu-budgie": "https://cdimage.ubuntu.com/ubuntu-budgie/releases/22.04/release/ubuntu-budgie-22.04.3-desktop-amd64.iso",
+    "ubuntu-mate": "https://cdimage.ubuntu.com/ubuntu-mate/releases/22.04/release/ubuntu-mate-22.04.3-desktop-amd64.iso",
+    "ubuntu-studio": "https://cdimage.ubuntu.com/ubuntustudio/releases/22.04/release/ubuntustudio-22.04.3-dvd-amd64.iso",
+    "edubuntu": "https://cdimage.ubuntu.com/edubuntu/releases/22.04/release/edubuntu-22.04.3-desktop-amd64.iso",
+    "kali-live": "https://cdimage.kali.org/kali-2023.3/kali-linux-2023.3-live-amd64.iso",
+    "kali-light": "https://cdimage.kali.org/kali-2023.3/kali-linux-light-2023.3-amd64.iso",
+    "kali-everything": "https://cdimage.kali.org/kali-2023.3/kali-linux-everything-2023.3-amd64.iso",
 }
 
 # Track VPS instances
@@ -54,6 +91,8 @@ def create_vps(os_name: str, ram: int, cpu: int):
     freeroot_dir = "/tmp/freeroot"
     if not os.path.exists(freeroot_dir):
         subprocess.run(["git", "clone", freeroot_repo, freeroot_dir], check=True)
+
+    # Run FreeRoot to customize the OS image
     subprocess.run(["python3", f"{freeroot_dir}/freeroot.py", "--image", os_image], check=True)
 
     # Step 3: Deploy the VPS using QEMU (replace with your preferred tool)
@@ -164,10 +203,6 @@ async def vps_status(ctx, vps_id: int):
         await ctx.send(f"✅ VPS {vps_id} is running.")
     else:
         await ctx.send(f"❌ VPS {vps_id} is not running.")
-
-# Run the bot
-if __name__ == "__main__":
-    bot.run(TOKEN)
 
 # Run the bot
 if __name__ == "__main__":
